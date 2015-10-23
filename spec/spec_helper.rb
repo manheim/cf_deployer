fb122c9590f05fcd70f6726b08322ac7da67af3e
Ryan Rosenblum <ryan.rosenblum@gmail.com>
2015-10-22 21:52:02 -0400

    Update Rspec configuration

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -7,3 +7,27 @@ def puts *args
 
 end
 
+RSpec.configure do |config|
+  config.example_status_persistence_file_path = 'spec/examples.txt'
+
+  config.expect_with :rspec do |expectations|
+    expectations.include_chain_clauses_in_custom_matcher_descriptions = true
+    expectations.syntax = :expect # Disable `should`
+  end
+
+  config.filter_run :focus
+
+  config.mock_with :rspec do |mocks|
+    mocks.syntax = :expect # Disable `should_receive` and `stub`
+    # Prevents you from mocking or stubbing a method that does not exist on
+    # a real object. This is generally recommended, and will default to
+    # `true` in RSpec 4.
+    # mocks.verify_partial_doubles = true # causing tests to fail in base_spec
+  end
+
+  # config.order = :random # There are tests failing in random order
+  config.run_all_when_everything_filtered = true
+  # config.warnings = true # way too many warnings right now
+
+  Kernel.srand config.seed
+end
