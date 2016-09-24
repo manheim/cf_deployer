82fb957e3b78db27518781eaad78ade3d09ef8eb
Keith Manning <keith.manning@manheim.com>
2016-09-24 15:39:55 -0400

    #39 - add helper method to ignore errors

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,7 +3,15 @@ Dir.glob("#{File.dirname File.absolute_path(__FILE__)}/fakes/*.rb") { |file| req
 
 CfDeployer::Log.log.outputters = nil
 
+RSPEC_LOG = Logger.new(STDOUT)
+RSPEC_LOG.level = Logger::INFO
+
 def puts *args
 
 end
 
+def ignore_errors
+  yield
+rescue => e
+  RSPEC_LOG.debug "Intentionally ignoring error: #{e.message}"
+end
